b811619b1559510a5740a5bf4f12599a570fce03
Your Name <you@example.com>
2021-05-29 21:44:14 -0400

    Lab 2 Complete

---
+++ README.md
@@ -0,0 +1,1 @@
+Muna Adan 040843043 adan0027@algonquinlive.com
---
+++ app/src/main/java/com/example/andriodlabs/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.andriodlabs;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.Switch;
+import android.widget.Toast;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_relative);
+
+        Button myButton = findViewById(R.id.myButton);
+        myButton.setOnClickListener( v -> Toast.makeText(MainActivity.this, getResources().getString(R.string.toast_message), Toast.LENGTH_LONG).show());
+
+        CheckBox myCheckBox = findViewById(R.id.checked);
+        myCheckBox.setOnCheckedChangeListener( (checkboxView, checked) -> {
+            Snackbar.make(checkboxView, checked ? R.string.checkedOn : R.string.checkedOff , Snackbar.LENGTH_LONG)
+                    .setAction(R.string.undo, v -> myCheckBox.setChecked(!checked))
+                    .show();
+        });
+
+        ImageButton imgButton = findViewById(R.id.imgButton);
+
+        Switch mySwitch = findViewById(R.id.switched);
+        mySwitch.setOnCheckedChangeListener((switchView, switchedOn) -> {
+            Snackbar.make(switchView, switchedOn ? R.string.switcedOn : R.string.switcedOff, Snackbar.LENGTH_LONG)
+                    .setAction(R.string.undo, v -> mySwitch.setChecked(!switchedOn))
+                    .show();
+
+        });
+
+
+    }
+}
